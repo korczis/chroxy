a8c18a5d2bcc7f5ea9f86357371eb9c7a1d72113
Tomas Korcak <korczis@gmail.com>
2020-01-03 15:14:16 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Chroxy.MixProject do
   defp deps do
     [
       {:plug, "~> 1.8.0"},
-      {:plug_cowboy, "~> 2.0.2"},
+      {:plug_cowboy, "~> 2.0"},
       {:cowboy, "~> 2.6.3"},
       {:jason, "~> 1.1"},
       {:erlexec, git: "https://github.com/saleyn/erlexec.git", override: true },
